c96261370c89d91fd6aefe520bfcfd9f26e49379
Jean Cesar <neo.jean@gmail.com>
2017-01-26 07:27:15 -0200

    Importacao de arquivo via ajax

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,65 +4,61 @@ class NegociacaoController{
 		let $ = document.querySelector.bind(document);
 
 		this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
+                this._inputQuantidade = $('#quantidade');
+                this._inputValor = $('#valor');
 
-        let self = this;
-        this._ListaNegociacoes = new Proxy(new ListaNegociacoes(), {
-        	get(target, prop, receiver) {
-        		if (['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                this._listaNegociacoes = new Bind(
+                        new ListaNegociacoes(),
+                        new NegociacoesView($("#negociacoesView")),
+                        'adiciona', 'esvazia');
 
-        			return function() {
-        				console.log(`Interceptando ${prop}`);
+                this._mensagem = new Bind(
+                        new Mensagem(),
+                        new MensagemView($("#mensagemView")),
+                        'texto');
+        }
 
-        				Reflect.apply(target[prop], target, arguments);
-        				self._NegociacoesView.update(target);
-        			}
-        		}
+        adicionar(event){
+              event.preventDefault();
 
-        		return Reflect.get(target, prop, receiver);
-        	}
-        });
+              this._listaNegociacoes.adiciona(this._CriaNegociacao());
+              this._mensagem.texto = 'Negociação adicionada com sucesso.';
+              this._LimpaFormulario();
+      }
 
-        this._NegociacoesView = new NegociacoesView($("#negociacoesView"));
-        this._NegociacoesView.update(this._ListaNegociacoes);
+      importaNegociacoes() {
+              let service = new NegociacaoService();
 
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($("#mensagemView"));
-        this._mensagemView.update(this._mensagem);
-	}
+              service.obterNegociacoesDaSemana((error, negociacoes) => {
+                      if(error) {
+                              this._mensagem.texto = error;
+                              return;
+                      }
 
-	adicionar(event){
-		event.preventDefault();
+                      negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
 
-        this._ListaNegociacoes.adiciona(this._CriaNegociacao());
-        this._LimpaFormulario();
+                      this._mensagem.texto = "Negociações importadas com sucesso."
+              });
+      }
 
-        this._mensagem.texto = 'Negociação adicionada com sucesso.';
-        this._mensagemView.update(this._mensagem);
-	}
+      apaga() {
+              this._listaNegociacoes.esvazia();
+              this._mensagem.texto = "Negociações apagadas como sucesso.";
+      }
 
-	apaga() {
-		this._ListaNegociacoes.esvazia();
+      _CriaNegociacao(){
+              return new Negociacao(
+               DateHelper.textoParaData(this._inputData.value),
+               this._inputQuantidade.value,
+               this._inputValor.value)	
+      }
+      
+      _LimpaFormulario(){
 
-		this._mensagem.texto = "Negociações apagadas como sucesso.";
-		this._mensagemView.update(this._mensagem);
-	}
-
-	_CriaNegociacao(){
-		return new Negociacao(
-        	DateHelper.textoParaData(this._inputData.value),
-        	this._inputQuantidade.value,
-        	this._inputValor.value
-        )	
-	}
-	
-	_LimpaFormulario(){
-
-		this._inputData.value = '';
-		this._inputQuantidade.value = 1;
-		this._inputValor.value = 0.0;
-		this._inputData.focus();
-	}
+              this._inputData.value = '';
+              this._inputQuantidade.value = 1;
+              this._inputValor.value = 0.0;
+              this._inputData.focus();
+      }
 
 }
